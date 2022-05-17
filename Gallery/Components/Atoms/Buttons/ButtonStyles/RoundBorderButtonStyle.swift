22d2e7102a68861f44aa68f72d01b9b2f0ac28d2
ukit-tokyo <t.yuki@ukit.tokyo>
2022-05-17 20:23:06 +0900

    Make round border button style #2

--- Gallery/Components/Atoms/Buttons/ButtonStyles/RoundBorderButtonStyle.swift
+++ Gallery/Components/Atoms/Buttons/ButtonStyles/RoundBorderButtonStyle.swift
@@ -1,5 +1,5 @@
 //
-//  RoundFillButtonStyle.swift
+//  RoundBorderButtonStyle.swift
 //  Gallery
 //
 //  Created by Taichi Yuki on 2022/05/17.
@@ -7,62 +7,69 @@
 
 import SwiftUI
 
-/// 塗り、角丸ボタンスタイル
-struct RoundFillButtonStyle<S: Shape>: ButtonStyle {
+/// 枠線、角丸ボタンスタイル
+struct RoundBorderButtonStyle<S: Shape>: ButtonStyle {
   struct Config {
     let backgroundColor: Color
     let foregroundColor: Color
+    let borderColor: Color
+    let borderWidth: CGFloat
     let font: Font
     let animated: Bool
-    
+
     init(
-      backgroundColor: Color = .primary,
-      foregroundColor: Color = .white,
+      backgroundColor: Color = .clear,
+      foregroundColor: Color = .primary,
+      borderColor: Color = .primary,
+      borderWidth: CGFloat = 2,
       font: Font,
       animated: Bool = true
     ) {
       self.backgroundColor = backgroundColor
       self.foregroundColor = foregroundColor
+      self.borderColor = borderColor
+      self.borderWidth = borderWidth
       self.font = font
       self.animated = animated
     }
   }
-  
+
   private let config: Config
   private let shape: S
   @Environment(\.isEnabled) var isEnabled: Bool
-  
+
   init(_ config: Config, shape: S) {
     self.config = config
     self.shape = shape
   }
-  
+
   func makeBody(configuration: Configuration) -> some View {
     let animateScale: CGFloat = config.animated ? 0.95 : 1
     let animateDuration: Double = config.animated ? 0.2 : 0
-    
-    return configuration.label
+
+    configuration.label
       .padding()
-      .background(isEnabled ? config.backgroundColor : Color.gray)
-      .foregroundColor(config.foregroundColor)
+      .background(config.backgroundColor)
+      .foregroundColor(isEnabled ? config.foregroundColor : Color.gray)
       .font(config.font)
-      .clipShape(shape)
+      .overlay(shape.stroke(isEnabled ? config.borderColor : Color.gray, lineWidth: config.borderWidth))
       .scaleEffect(configuration.isPressed ? animateScale : 1)
       .animation(.easeOut(duration: animateDuration))
   }
 }
 
-struct RoundFillButtonStyle_Previews: PreviewProvider {
-  static let roundButton: some View = Button("Round Fill") {}
-    .buttonStyle(RoundFillButtonStyle(
+struct RoundBorderButtonStyle_Previews: PreviewProvider {
+  static let roundButton: some View = Button("Round Border") {}
+    .buttonStyle(RoundBorderButtonStyle(
       .init(font: .body.bold()),
       shape: RoundedRectangle(cornerRadius: 10)
     ))
-  static let capsuleButton: some View = Button("Capsule Fill") {}
-    .buttonStyle(RoundFillButtonStyle(
+  static let capsuleButton: some View = Button("Capsule Border") {}
+    .buttonStyle(RoundBorderButtonStyle(
       .init(font: .body.bold()),
       shape: Capsule()
     ))
+
   static var previews: some View {
     Group {
       roundButton
@@ -70,7 +77,7 @@ struct RoundFillButtonStyle_Previews: PreviewProvider {
     }
     .previewLayout(.fixed(width: 200, height: 70))
     .preferredColorScheme(.light)
-    
+
     Group {
       roundButton
       capsuleButton
