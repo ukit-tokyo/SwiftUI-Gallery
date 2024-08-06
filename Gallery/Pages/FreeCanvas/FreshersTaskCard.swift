7ab0d8907a245d2cb3533f5e3b2f31d58afb6c4d
ukit-tokyo <t.yuki@ukit.tokyo>
2024-08-06 20:23:57 +0900

    add achieved status

--- Gallery/Pages/FreeCanvas/FreshersTaskCard.swift
+++ Gallery/Pages/FreeCanvas/FreshersTaskCard.swift
@@ -16,6 +16,8 @@ struct FreshersTaskCard: View {
   let taskReward: Int
   let taskProgress: (value: Int, total: Int)
 
+  var allAchieved: Bool { totalProgress.value >= totalProgress.total }
+
   var body: some View {
     VStack(spacing: 12) {
 
@@ -28,26 +30,31 @@ struct FreshersTaskCard: View {
             .typography(font: .systemFont(ofSize: 12), lineHeight: 16)
             .lineLimit(2)
             .foregroundStyle(.black)
-          Text("完了まで: \(totalProgress.value)/\(totalProgress.total)")
-            .typography(font: .boldSystemFont(ofSize: 12), lineHeight: 30)
-            .foregroundStyle(.gray)
+          if !allAchieved {
+            Text("完了まで: \(totalProgress.value)/\(totalProgress.total)")
+              .typography(font: .boldSystemFont(ofSize: 12), lineHeight: 30)
+              .foregroundStyle(.gray)
+          }
         }
 
-        CompleteButton()
+        Spacer()
+
+        CompleteButton(reward: reward, achieved: allAchieved)
       }
       .padding(.horizontal, 16)
 
-      Divider()
-        .frame(maxWidth: .infinity)
-        .background(.gray)
+      if !allAchieved {
+        Divider()
+          .frame(maxWidth: .infinity)
+          .background(.gray)
 
-      TaskCard(
-        title: taskTitle,
-        reward: taskReward,
-        progress: taskProgress
-      )
+        TaskCard(
+          title: taskTitle,
+          reward: taskReward,
+          progress: taskProgress
+        )
+      }
     }
-    .fixedSize()
     .padding(.vertical, 12)
     .background(.gray.opacity(0.1))
     .clipShape(RoundedRectangle(cornerRadius: 10))
@@ -56,27 +63,38 @@ struct FreshersTaskCard: View {
 
 extension FreshersTaskCard {
   private struct CompleteButton: View {
+    let reward: Int
+    let achieved: Bool
+
     var body: some View {
       ZStack {
-        VStack(spacing: 4) {
+        VStack(spacing: 2) {
           Text("すべて完了")
             .typography(font: .boldSystemFont(ofSize: 10), lineHeight: 12)
-            .foregroundStyle(.gray)
-          Image("")
-            .resizable()
-            .frame(width: 24, height: 24)
-            .background(.gray)
-            .clipShape(Circle())
-          Text("30")
-            .typography(font: .boldSystemFont(ofSize: 10), lineHeight: 16)
-            .foregroundStyle(.gray)
+            .foregroundStyle(achieved ? .white : .gray)
+
+          VStack(spacing: 0) {
+            Image("")
+              .resizable()
+              .frame(width: 24, height: 24)
+              .background(.gray)
+              .clipShape(Circle())
+            Text(String(reward))
+              .typography(font: .boldSystemFont(ofSize: 10), lineHeight: 16)
+              .foregroundStyle(achieved ? .black : .gray)
+          }
+          .padding(EdgeInsets(top: 0, leading: 10, bottom: 0, trailing: 10))
+          .background(.white)
+          .clipShape(RoundedRectangle(cornerRadius: 4))
         }
         .padding(8)
-        .background(.white)
+        .background(achieved ? .red : .white)
 
-        RoundedRectangle(cornerRadius: 8)
-          .stroke(style: StrokeStyle(lineWidth: 1, dash: [4, 4]))
-          .foregroundStyle(.gray)
+        if !achieved {
+          RoundedRectangle(cornerRadius: 8)
+            .stroke(style: StrokeStyle(lineWidth: 1, dash: [4, 4]))
+            .foregroundStyle(.gray)
+        }
       }
       .fixedSize()
       .clipShape(RoundedRectangle(cornerRadius: 8))
@@ -87,7 +105,7 @@ extension FreshersTaskCard {
     let title: String
     let reward: Int
     let progress: (value: Int, total: Int)
-    var completed: Bool { progress.value >= progress.total }
+    var achieved: Bool { progress.value >= progress.total }
 
     var body: some View {
       HStack(alignment: .center, spacing: 12) {
@@ -122,10 +140,17 @@ extension FreshersTaskCard {
           }
         }
 
-        FillTextButton("トライ", theme: .custom(backgroundColor: .black, foregroundColor: .white), font: .system(size: 12).bold(), height: 24) {
-          print("try it!")
+        if achieved {
+          FillTextButton("受け取る", theme: .custom(backgroundColor: .red, foregroundColor: .white), font: .system(size: 12).bold(), height: 24) {
+            print("achieved!")
+          }
+          .padding(0)
+        } else {
+          FillTextButton("トライ", theme: .custom(backgroundColor: .black, foregroundColor: .white), font: .system(size: 12).bold(), height: 24) {
+            print("try it!")
+          }
+          .padding(0)
         }
-        .padding(0)
       }
       .padding(.horizontal, 16)
     }
@@ -165,5 +190,7 @@ extension View {
     reward: 30,
     taskTitle: "いいねを５回しよう",
     taskReward: 10,
-    taskProgress: (value: 1, total: 5))
+    taskProgress: (value: 1, total: 5)
+  )
+  .padding()
 }
