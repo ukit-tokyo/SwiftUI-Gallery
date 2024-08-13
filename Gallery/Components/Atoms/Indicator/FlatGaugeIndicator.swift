8049cab242db9d4a633d2fc71081d8c295ad11c5
ukit-tokyo <t.yuki@ukit.tokyo>
2024-08-13 16:31:15 +0900

    create flat gauge indicator

---
+++ Gallery/Components/Atoms/Indicator/FlatGaugeIndicator.swift
@@ -0,0 +1,54 @@
+//
+//  FlatPageIndicator.swift
+//  Gallery
+//
+//  Created by Taichi Yuki on 2024/08/13.
+//
+
+import SwiftUI
+
+struct FlatGaugeIndicator: View {
+  let height: CGFloat
+  let stepCount: Int
+  let step: Int
+
+  @State private var totalWidth: CGFloat = .zero
+  private var offsetX: CGFloat {
+    (totalWidth / CGFloat(stepCount)) * CGFloat(step)
+  }
+
+  var body: some View {
+    ZStack(alignment: .leading) {
+      Capsule()
+        .fill(.gray)
+        .frame(maxWidth: .infinity, maxHeight: height)
+        .background(
+          GeometryReader { geometry in
+            Capsule()
+              .frame(width: geometry.size.width, height: geometry.size.height)
+              .onAppear {
+                totalWidth = geometry.size.width
+              }
+          }
+        )
+
+      Capsule()
+        .fill(.black)
+        .frame(width: totalWidth / CGFloat(stepCount), height: height)
+        .offset(x: offsetX)
+    }
+    .mask {
+      HStack(spacing: 8) {
+        ForEach(0..<stepCount, id: \.self) { _ in
+          Capsule().frame(height: height)
+        }
+      }
+    }
+  }
+}
+
+@available(iOS 17.0, *)
+#Preview(traits: .sizeThatFitsLayout) {
+  FlatGaugeIndicator(height: 8, stepCount: 3, step: 1)
+    .padding()
+}
